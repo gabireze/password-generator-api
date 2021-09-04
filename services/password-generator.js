75403c755417d8793c75c4e27b6f7696063d65c6
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-09-04 20:48:02 -0300

    added size restrictions

--- services/password-generator.js
+++ services/password-generator.js
@@ -3,6 +3,11 @@ const generator = require("generate-password");
 class PasswordGeneratorService {
   async generate(options) {
     try {
+      if (options.length) {
+        if (options.length > 100) {
+          options.length = 100;
+        }
+      }
       const password = generator.generate(options);
       return password;
     } catch (error) {
@@ -12,6 +17,16 @@ class PasswordGeneratorService {
 
   async generateMultiple(amount, options) {
     try {
+      if (options.length) {
+        if (options.length > 100) {
+          options.length = 100;
+        }
+      }
+      if (amount) {
+        if (amount > 100) {
+          amount = 100;
+        }
+      }
       const password = generator.generateMultiple(amount, options);
       return password;
     } catch (error) {
